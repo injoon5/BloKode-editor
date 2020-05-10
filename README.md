8e40792f28737266350ae42cbbab92caccc2c9fa
thoratica <admin@thoratica.net>
2020-05-10 23:28:16 +0900

    Change to VanillaJS

--- README.md
+++ README.md
@@ -6,8 +6,8 @@
 
 ## EN
 BloKode is an open-source block coding service.  
-It's uses jQuery, so if you hate jQuery, just do `history.back()`
+It's uses VanillaJS, so if you are not good at VanillaJS, just do `history.back()`
 
 ## KR
 BloKode는 오픈소스 블록 코딩 서비스입니다.  
-jQuery를 사용하므로, 만약 jQuery를 혐오하신다면 `history.back()` 을 해주세요.
+VanillaJS를 사용하므로, 만약 VanillaJS를 못하신다면 `history.back()` 을 해주세요.
--- editor/assets/js/main.js
+++ editor/assets/js/main.js
@@ -1,12 +1,61 @@
-(async() => {
-    const lang = new Object
-    const kr = await (await fetch('lang/kr.js')).text()
-    const en = await (await fetch('lang/en.js')).text()
-    eval(`${kr}\n${en}`)
-    const bloKode = await (await fetch('blokode.html')).text()
-    const bloKodeJS = await (await fetch('assets/js/blokode.js')).text()
-    const bloKodeBlock = await (await fetch('assets/js/blocks.js')).text()
-    document.getElementById('blokode').innerHTML = bloKode
-    eval(`${bloKodeJS}\n${bloKodeBlock}`)
-    tmripple.init()
-})()
+const blokode = document.createElement('div')
+
+const parseHTML = (html) => {
+    let t = document.createElement('template');
+    t.innerHTML = html;
+    return t.content.cloneNode(true);
+}
+
+const BloKode = {
+    components: {
+        header: parseHTML('<header><div class="headerInner"><div class="logo"><a href="/">BloKode</a><a href="/new">editor</a></div><button>test</button></div></header>'),
+        blockList: parseHTML('<div class="blockList"></div>'),
+        playground: parseHTML('<div class="playground"></div>'),
+    },
+    block: {
+        event: (c) => {
+            return parseHTML(`<div class="block event">${c}</div>`)
+        },
+        basic: (c) => {
+            return parseHTML(`<div class="block basic">${c}</div>`)
+        },
+    },
+}
+
+blokode.append(BloKode.components.header)
+blokode.getElementsByTagName('header')[0].append(BloKode.components.blockList)
+blokode.append(BloKode.components.playground)
+
+let playground = blokode.getElementsByClassName('playground')[0],
+    blockList = blokode.getElementsByClassName('blockList')[0]
+
+blockList.append(BloKode.block.event('test'))
+blockList.append(BloKode.block.basic('test'))
+blockList.append(BloKode.block.basic('tes'))
+
+blokode.getElementsByTagName('a')[0].style.fontSize = '2rem'
+blokode.getElementsByTagName('a')[1].style.fontSize = '1.8rem'
+blokode.getElementsByTagName('a')[0].style.color = '#3C82F4'
+blokode.getElementsByTagName('a')[1].style.color = '#000000'
+blokode.getElementsByTagName('a')[1].style.marginLeft = '5px'
+
+document.getElementById('blokode').append(blokode)
+
+window.addEventListener('load', () => {
+    blokode.getElementsByClassName('playground')[0].style.marginTop = `${blokode.getElementsByTagName('header')[0].clientHeight + 15}px`
+})
+
+new Sortable(blockList, {
+    group: {
+        name: 'shared',
+        pull: 'clone',
+        put: false
+    },
+    animation: 150,
+    sort: false
+})
+
+new Sortable(playground, {
+    group: 'shared',
+    animation: 150
+})
