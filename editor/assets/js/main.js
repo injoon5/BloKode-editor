092d1e03759d2d83f192e214f0cb85dea8a78ac5
thoratica <admin@thoratica.net>
2020-05-12 12:42:28 +0900

    ㅎㅎ

--- editor/assets/js/main.js
+++ editor/assets/js/main.js
@@ -11,6 +11,7 @@ const BloKode = {
         header: parseHTML('<header><div class="headerInner"><div class="logo"><a href="/">BloKode</a><a href="/editor">editor</a></div><button class="run">실행</button></div></header>'),
         blockList: parseHTML('<div class="blockList"></div>'),
         playground: parseHTML('<div class="playground"></div>'),
+        context: parseHTML('<div class="context"></div>'),
     },
     block: {
         event: (c) => {
@@ -37,6 +38,20 @@ let playground = blokode.getElementsByClassName('playground')[0],
 
     blokode.getElementsByClassName('playground')[0].style.marginTop = `${blokode.getElementsByTagName('header')[0].clientHeight + 15}px`
     blokode.getElementsByClassName('playground')[0].style.height = `calc(100vh - ${blokode.getElementsByTagName('header')[0].clientHeight + 15}px)`
+
+    for (let i = 0; i < blokode.getElementsByClassName('block').length; i++) {
+        blokode.getElementsByClassName('block')[i].addEventListener('contextmenu', (e) => {
+            e.preventDefault()
+            blokode.append(BloKode.components.context)
+            blokode.getElementsByClassName('context')[0].style.left = `${e.clientX}px`
+            blokode.getElementsByClassName('context')[0].style.top = `${e.clientY}px`
+        })
+    }
+    document.querySelectorAll('div').forEach((el) => {
+        el.addEventListener('click', () => {
+            console.log(el)
+        })
+    })
 })()
 
 blokode.getElementsByTagName('a')[0].style.fontSize = '2rem'
@@ -44,6 +59,8 @@ blokode.getElementsByTagName('a')[1].style.fontSize = '1.8rem'
 blokode.getElementsByTagName('a')[0].style.color = '#3C82F4'
 blokode.getElementsByTagName('a')[1].style.color = '#000000'
 blokode.getElementsByTagName('a')[1].style.marginLeft = '5px'
+
+playground.append(parseHTML('<div class="noblock"><div class="title">아직 블록이 없어요!</div><div class="content">상단에 있는 초록색 이벤트 블록을 끌어와 시작해보세요.</div></div>'))
 
 document.getElementById('blokode').append(blokode)
 
@@ -61,6 +78,11 @@ new Sortable(blockList, {
         name: 'shared',
         pull: 'clone',
         put: false
+    },
+    onStart: (e) => {
+        if (typeof blokode.getElementsByClassName('noblock')[0] != 'undefined') {
+            blokode.getElementsByClassName('noblock')[0].remove()
+        }
     },
     animation: 150,
     sort: false
@@ -86,7 +108,6 @@ new Sortable(playground, {
 
         new Sortable(target.getElementsByClassName('blocks')[0], {
             group: 'shared',
-            animation: 150,
             onAdd: (e) => {
                 if (e.clone.classList[1] == 'event') {
                     e.item.remove()
@@ -94,5 +115,5 @@ new Sortable(playground, {
                 }
             }
         })
-    }
+    },
 })
