8aa4b446cc8d13adf4ff7249bee2f597ba2fe91e
injoon5 <52849281+injoon5@users.noreply.github.com>
2020-07-11 21:40:06 +0900

    Update blocks.js

--- editor/assets/js/blocks.js
+++ editor/assets/js/blocks.js
@@ -20,4 +20,16 @@ BloKode.block.scripts = {
             alert(input)
         },
     },
+     popup: {
+        skeleton: 'basic',
+        template: '팝업창 열기. URL:<input value="about:blank"> 제목:<title value="Hello, world!">',
+        func: (input) => {
+            function popup(){
+            var url = input;
+            var name = title;
+            var option = "width = 500, height = 500, top = 100, left = 200, location = no"
+            window.open(url, name, option);
+        }
+        },
+    },
 }
